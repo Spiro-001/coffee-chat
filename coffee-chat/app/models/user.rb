a3eeffd6ca16bf15df76580cf46f79490eec3876
Yong Kim <yhkim4573@gmail.com>
2022-12-04 00:18:15 -0500

    Active Records : [UM] added validations for email, phone_number, password, session_token, 4 methods added, self.find_by_credentials(credential, password), reset_session_token, generate_unique_session_token, ensure_session_token

--- coffee-chat/app/models/user.rb
+++ coffee-chat/app/models/user.rb
@@ -11,5 +11,35 @@
 #  updated_at    :datetime         not null
 #
 class User < ApplicationRecord
+    has_secure_password
+    before_validation :ensure_session_token
+
     validates :email, presence: true, uniqueness: { message: "Someone’s already using that email." }, format: { with: URI::MailTo::EMAIL_REGEXP } # bottom right motto pop-up.
+    validates :phone_number, phone: true, uniqueness: { message: "Someone's already using that phone-number" }
+    validates :password, allow_nil: true, length : { minimum: 6, maximum: 16 }
+    validates :session_token, presence: true, uniqueness: true
+
+    def self.find_by_credentials(credential, password)
+        username_or_email = credential.match?(URI::MailTo::EMAIL_REGEXP) ? User.find_by(email: credential) : User.find_by(username: credential)
+        return nil if username_or_email.nil?
+        username_or_email.authenticate(password) ? username_or_email : nil
+    end
+
+    def reset_session_token!
+        self.update!(:session_token => generate_unique_session_token)
+        self.session_token
+    end
+
+    private
+    def generate_unique_session_token
+        session_token = SecureRandom.base64
+        while User.exists?(:session_token => session_token)
+            session_token = SecureRandom.base64
+        end
+        return session_token
+    end
+
+    def ensure_session_token
+        self.session_token ||= generate_unique_session_token
+    end
 end
