d2bb2962aa57604ac37b484a0cbe16af7ba86044
Yong Kim <yhkim4573@gmail.com>
2022-12-03 23:58:53 -0500

    Active Records : [UM] password_digest renamed password

---
+++ coffee-chat/db/migrate/20221204045622_remove_password_digest_user.rb
@@ -0,0 +1,5 @@
+class RemovePasswordDigestUser < ActiveRecord::Migration[7.0]
+  def change
+    rename_column :users, :password_digest, :password
+  end
+end
--- coffee-chat/db/schema.rb
+++ coffee-chat/db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_04_044532) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_04_045622) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
     t.string "phone_number", null: false
-    t.string "password_digest", null: false
+    t.string "password", null: false
     t.string "session_token", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
