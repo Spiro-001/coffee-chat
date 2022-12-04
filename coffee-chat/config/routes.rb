aac532906bb294d006113c5447482aea3bc6f0f6
Yong Kim <yhkim4573@gmail.com>
2022-12-04 01:01:01 -0500

    Frontend : store added in src

--- coffee-chat/config/routes.rb
+++ coffee-chat/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  namespace :api, default: { format :json } do
+  namespace :api, default: { format: :json } do
     resources :users, only: :create
     resource :session, only: [:show, :create, :destroy]
   end
