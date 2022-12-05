5e8014f4537222afc0e158938fcdad0b83b34a96
Yong Kim <yhkim4573@gmail.com>
2022-12-05 17:16:42 -0500

    Prep for render

--- coffee-chat/config/routes.rb
+++ coffee-chat/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get '*path', to: "static_pages#frontend_index"
   namespace :api, defaults: { format: :json } do
     resources :users, only: :create
     resource :session, only: [:show, :create, :destroy]
   end
+  get '*path', to: "static_pages#frontend_index"
 end
