55833af49e0df9209a661282e9d26453c723d752
Yong Kim <yhkim4573@gmail.com>
2022-12-06 15:07:16 -0500

    more css

--- coffee-chat/frontend/src/components/Feed.js
+++ coffee-chat/frontend/src/components/Feed.js
@@ -1,10 +1,12 @@
-import { useSelector } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import { Redirect } from "react-router-dom"
 import { LoginForm } from "./LoginForm"
 
 export const Feed = () => {
 
+    const dispatch = useDispatch();
     const currentUser = useSelector(state => state.session.user)
+    
     if (!currentUser) return <Redirect to="/"/>
     return (
         <div>
