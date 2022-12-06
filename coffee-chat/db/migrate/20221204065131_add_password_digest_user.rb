bdf3e87e6810bdfff4fe3f84fe7c3aa0b7bd94f3
Yong Kim <yhkim4573@gmail.com>
2022-12-06 11:04:51 -0500

    migration edit

--- coffee-chat/db/migrate/20221204065131_add_password_digest_user.rb
+++ coffee-chat/db/migrate/20221204065131_add_password_digest_user.rb
@@ -1,6 +1,5 @@
 class AddPasswordDigestUser < ActiveRecord::Migration[7.0]
   def change
-    remove_column :users, :passsword
     add_index :users, :session_token
     change_column :users, :session_token, :string, null: false
   end
